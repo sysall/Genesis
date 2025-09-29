58160aab4b42f66f036ffb1cebf635ef7f4d29a9
sy sall <50021226+sysall@users.noreply.github.com>
2025-09-29 14:45:09 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Services from './components/Services';
 import Portfolio from './components/Portfolio';
-import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
-import Footer from './components/Footer';
 
 function App() {
   return (
@@ -16,9 +14,7 @@ function App() {
       <About />
       <Services />
       <Portfolio />
-      <Testimonials />
       <Contact />
-      <Footer />
     </div>
   );
 }
